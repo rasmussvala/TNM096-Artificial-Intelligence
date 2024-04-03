6c86cf8fdae67859ba0303dc8cb4653b86751943
rasmussvala <rassv453@student.liu.se>
2024-04-03 20:58:01 +0200

    changed from arr to min heap

--- a-star.js
+++ a-star.js
@@ -1,6 +1,116 @@
 // Define the NxN size
 const N = 3;
 
+// taken from https://www.geeksforgeeks.org/min-heap-in-javascript/
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  // Helper Methods
+  getLeftChildIndex(parentIndex) {
+    return 2 * parentIndex + 1;
+  }
+  getRightChildIndex(parentIndex) {
+    return 2 * parentIndex + 2;
+  }
+  getParentIndex(childIndex) {
+    return Math.floor((childIndex - 1) / 2);
+  }
+  hasLeftChild(index) {
+    return this.getLeftChildIndex(index) < this.heap.length;
+  }
+  hasRightChild(index) {
+    return this.getRightChildIndex(index) < this.heap.length;
+  }
+  hasParent(index) {
+    return this.getParentIndex(index) >= 0;
+  }
+  leftChild(index) {
+    return this.heap[this.getLeftChildIndex(index)];
+  }
+  rightChild(index) {
+    return this.heap[this.getRightChildIndex(index)];
+  }
+  parent(index) {
+    return this.heap[this.getParentIndex(index)];
+  }
+
+  // Functions to create Min Heap
+
+  swap(indexOne, indexTwo) {
+    const temp = this.heap[indexOne];
+    this.heap[indexOne] = this.heap[indexTwo];
+    this.heap[indexTwo] = temp;
+  }
+
+  peek() {
+    if (this.heap.length === 0) {
+      return null;
+    }
+    return this.heap[0];
+  }
+
+  // Removing an element will remove the
+  // top element with highest priority then
+  // heapifyDown will be called
+  remove() {
+    if (this.heap.length === 0) {
+      return null;
+    }
+    const item = this.heap[0];
+    this.heap[0] = this.heap[this.heap.length - 1];
+    this.heap.pop();
+    this.heapifyDown();
+    return item;
+  }
+
+  add(item) {
+    this.heap.push(item);
+    this.heapifyUp();
+  }
+
+  // Modified heapifyUp to compare nodes based on their cost
+  heapifyUp() {
+    let index = this.heap.length - 1;
+    while (
+      this.hasParent(index) &&
+      this.parent(index).cost > this.heap[index].cost
+    ) {
+      this.swap(this.getParentIndex(index), index);
+      index = this.getParentIndex(index);
+    }
+  }
+
+  // Modified heapifyDown to compare nodes based on their cost
+  heapifyDown() {
+    let index = 0;
+    while (this.hasLeftChild(index)) {
+      let smallerChildIndex = this.getLeftChildIndex(index);
+      if (
+        this.hasRightChild(index) &&
+        this.rightChild(index).cost < this.leftChild(index).cost
+      ) {
+        smallerChildIndex = this.getRightChildIndex(index);
+      }
+      if (this.heap[index].cost < this.heap[smallerChildIndex].cost) {
+        break;
+      } else {
+        this.swap(index, smallerChildIndex);
+      }
+      index = smallerChildIndex;
+    }
+  }
+
+  printHeap() {
+    var heap = ` ${this.heap[0]} `;
+    for (var i = 1; i < this.heap.length; i++) {
+      heap += ` ${this.heap[i]} `;
+    }
+    console.log(heap);
+  }
+}
+
 class Node {
   constructor(matrix, x, y, moves, parent) {
     this.parent = parent;
@@ -48,19 +158,17 @@ export function solve(initial, final) {
 
   [x, y] = findZeroCoordinate(initial);
 
-  const openL = [];
+  const openL = new MinHeap();
   const closeL = new Set();
 
   const root = new Node(initial, x, y, 0, null);
   root.cost = calculateCost(root.matrix, final);
 
-  openL.push(root);
+  openL.add(root);
 
-  while (openL.length > 0) {
-    openL.sort((a, b) => a.cost + a.moves - b.cost + b.moves);
-
+  while (openL.heap.length > 0) {
     // Removes the node with the lowest cost
-    const min = openL.shift();
+    const min = openL.remove();
 
     if (min.cost === 0) {
       printPath(min);
@@ -71,7 +179,9 @@ export function solve(initial, final) {
 
     let arrayWithMoves = validMoves(min, final, closeL);
 
-    openL.push(...arrayWithMoves);
+    for (let i = 0; i < arrayWithMoves.length; i++) {
+      openL.add(arrayWithMoves[i]);
+    }
   }
 }
 
--- main.js
+++ main.js
@@ -1,9 +1,29 @@
 import { solve } from "./a-star.js";
 
-const initial = [
+const test = [
+  [3, 1, 6],
+  [2, 7, 5],
+  [4, 8, 0],
+];
+
+const test2 = [
   [1, 2, 3],
   [4, 5, 6],
   [7, 0, 8],
+  [0, 7, 8],
+];
+
+// Try these below!
+const hard1 = [
+  [6, 4, 7],
+  [8, 5, 0],
+  [3, 2, 1],
+];
+
+const hard2 = [
+  [8, 6, 7],
+  [2, 5, 4],
+  [3, 0, 1],
 ];
 
 const final = [
@@ -12,4 +32,4 @@ const final = [
   [7, 8, 0],
 ];
 
-solve(initial, final);
+solve(hard2, final);
